aa033fc99a87bd5c244dee848904eb344774767f
DashaSlesareva <zalezka@yandex.ru>
2022-06-16 14:03:45 +0300

    fixes #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
         long amount = 1000_60;
         boolean registered = true;
+        long result = service.calculate(amount, registered);
 
-        System.out.println(service.calculate(amount, registered));
+        System.out.println(result);
     }
 }
